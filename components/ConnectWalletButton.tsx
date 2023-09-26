ec4120689d3c64a3d5eb80f3252ff70f0f57a6a8
sotatek-stephen-pham <145954854+sotatek-stephen-pham@users.noreply.github.com>
2023-09-26 15:24:32 +0700

    [ECO-506] Building responsive navbar (#4)
    [ECO-506] Building responsive navbar

---
+++ components/ConnectWalletButton.tsx
@@ -0,0 +1,18 @@
+import { jost } from '@/fonts/fonts'
+import React from 'react'
+
+interface ConnectWalletButtonProps {
+    responsive?: boolean
+}
+
+const ConnectWalletButton = ({ responsive }: ConnectWalletButtonProps) => {
+    return (
+        <button
+            className={`${responsive ? "hidden lg:block" : ""} bg-white text-800 text-12 py-1 px-4 whitespace-nowrap h-fit w-fit leading-30 font-bold ${jost.variable} font-jost`}
+        >
+            Connect Wallet
+        </button>
+    )
+}
+
+export default ConnectWalletButton
