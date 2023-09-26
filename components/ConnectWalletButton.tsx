eafde87908505e1084daad82bc21c7a05c74b626
sotatek-tyler-nguyen <tyler.nguyen3@sotatek.com>
2023-09-26 18:03:50 +0700

    feat: modal component

--- components/ConnectWalletButton.tsx
+++ components/ConnectWalletButton.tsx
@@ -1,17 +1,33 @@
 import { jost } from '@/fonts/fonts'
 import React from 'react'
+import { useBoolean } from 'usehooks-ts'
+import Modal from './modal'
+
 
 interface ConnectWalletButtonProps {
     responsive?: boolean
 }
 
 const ConnectWalletButton = ({ responsive }: ConnectWalletButtonProps) => {
+    const { value: openModal, setFalse: setClose, setTrue: setOpen } = useBoolean(false)
+
     return (
-        <button
-            className={`${responsive ? "hidden lg:block" : ""} bg-white text-800 text-12 py-1 px-4 whitespace-nowrap h-fit w-fit leading-30 font-bold ${jost.variable} font-jost`}
-        >
-            Connect Wallet
-        </button>
+        <>
+            <button
+                onClick={setOpen}
+                className={`${responsive ? "hidden lg:block" : ""} bg-white text-800 text-12 py-1 px-4 whitespace-nowrap h-fit w-fit leading-30 font-bold ${jost.variable} font-jost`}
+            >
+                Connect Wallet
+            </button>
+            <Modal
+                className='md:w-1/2 lg:w-1/4'
+                open={openModal}
+                onClose={setClose}
+            >
+
+            </Modal>
+        </>
+
     )
 }
 
