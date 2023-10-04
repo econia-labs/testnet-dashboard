f6474c38dac8f3db5a6c9dc986a4e39ce6493a22
sotatek-stephen-pham <stephen.pham@sotatek.com>
2023-10-04 14:26:48 +0700

    Fix Econia logo onClick

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,18 +4,25 @@ import { mono } from '@/fonts/fonts'
 import Loader from './loader'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    const [animationFinished, setAnimationFinished] = useState(false)
+    const [animationFinished, setAnimationFinished] = useState(false);
+    const [loaderShow, setLoaderShow] = useState(true);
 
     useEffect(() => {
-        const timer = setTimeout(() => {
+        const animationTimer = setTimeout(() => {
             setAnimationFinished(true)
         }, 3000);
-        return () => clearTimeout(timer);
+        const loaderTimer = setTimeout(() => {
+            setLoaderShow(false)
+        }, 5000);
+        return () => {
+            clearTimeout(animationTimer);
+            clearTimeout(loaderTimer);
+        }
     }, [])
 
     return (
         <div className={`${mono.variable} font-mono text-white pt-35 pl-46 pr-31.79 sm:pr-33.71 md:pr-42 lg:px-105`}>
-            <Loader animationFinished={animationFinished} />
+            {loaderShow && <Loader animationFinished={animationFinished} />}
             <Navbar />
             {children}
         </div>
--- components/loader.tsx
+++ components/loader.tsx
@@ -4,7 +4,7 @@ import { mono } from '@/fonts/fonts'
 
 const Loader = ({ animationFinished }: { animationFinished: boolean }) => {
     return (
-        <div className={`fixed top-0 left-0 pointer-events-none w-full h-full bg-800 bg-noise grid place-items-center z-[1] transition-opacity duration-2000 ${animationFinished ? "opacity-0" : ""}`}>
+        <div className={`fixed top-0 left-0 w-full h-full bg-800 bg-noise grid place-items-center z-[1] transition-opacity duration-2000 ${animationFinished ? "opacity-0" : ""}`}>
             <div className='flex flex-col items-center gap-30'>
                 <Image className='w-[150px] h-auto' src={logo1} alt="logo" />
                 <div className={`${mono.variable} font-mono text-white uppercase text-24 leading-20 font-light typewriter`}>
