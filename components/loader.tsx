5bf07a50784382f9ee0931f80d929f617e9a85f2
sotatek-stephen-pham <145954854+sotatek-stephen-pham@users.noreply.github.com>
2023-10-04 15:03:35 +0700

    Fix intro text glitch
    https://linear.app/econia-labs/issue/ECO-583/video-intro-text-glitches-to-the-bottom-mobile

--- components/loader.tsx
+++ components/loader.tsx
@@ -7,7 +7,7 @@ const Loader = ({ animationFinished }: { animationFinished: boolean }) => {
         <div className={`fixed top-0 left-0 w-full h-full bg-800 bg-noise grid place-items-center z-[1] transition-opacity duration-2000 ${animationFinished ? "opacity-0" : ""}`}>
             <div className='flex flex-col items-center gap-30'>
                 <Image className='w-[150px] h-auto' src={logo1} alt="logo" />
-                <div className={`${mono.variable} font-mono text-white uppercase text-24 leading-20 font-light typewriter`}>
+                <div className={`${mono.variable} font-mono text-white uppercase text-24 leading-20 font-light typewriter whitespace-nowrap sm:whitespace-normal`}>
                 </div>
             </div>
         </div>
