bf46fe0106b2933a71c65846e64c97d7450d1519
sotatek-stephen-pham <stephen.pham@sotatek.com>
2023-10-05 13:57:03 +0700

    Adjust leaderboard layout

--- containers/leaderboard/index.tsx
+++ containers/leaderboard/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import CountDown from '@/components/count-down'
-import LeaderboardStats from '@/components/leaderboard-stats'
-import LeaderboardTable from '@/components/leaderboard-table'
+import React from "react";
+import CountDown from "@/components/count-down";
+import LeaderboardStats from "@/components/leaderboard-stats";
+import LeaderboardTable from "@/components/leaderboard-table";
 
 const LeaderBoardContainer = () => {
-    return (
-        <div className='flex flex-col items-center w-317 sm:w-437 md:w-605 lg:w-757 m-auto max-h-[calc(100vh-73.3px)]'>
-            <div className='mt-58'><CountDown /></div>
-            <div className='mt-44 hidden md:flex'><LeaderboardStats /></div>
-            <div className='mt-42 md:mt-52 lg:mt-36 grow overflow-y-scroll'><LeaderboardTable /></div>
-            <div className="fixed bottom-0 w-full h-290 bg-gradient-to-b from-transparent to-black pointer-events-none"></div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col items-center w-317 sm:w-437 md:w-605 lg:w-757 m-auto max-h-[calc(100vh-71.3px)]">
+      <div className="mt-58">
+        <CountDown />
+      </div>
+      <div className="mt-44 hidden md:flex">
+        <LeaderboardStats />
+      </div>
+      <div className="mt-42 md:mt-52 lg:mt-36 grow overflow-y-scroll">
+        <LeaderboardTable />
+      </div>
+      <div className="fixed bottom-0 w-full h-290 bg-gradient-to-b from-transparent to-black pointer-events-none"></div>
+    </div>
+  );
+};
 
-export default LeaderBoardContainer
+export default LeaderBoardContainer;
