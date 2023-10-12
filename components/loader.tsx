fab61f5770adfd26d0d695061eb9991ee87c8241
sotatek-stephen-pham <stephen.pham@sotatek.com>
2023-10-12 15:33:38 +0700

    Add fade into view effect for econia logo

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import logo1 from "../public/logo1.svg";
 import { mono } from "@/fonts/fonts";
 
 const Loader = ({ animationFinished }: { animationFinished: boolean }) => {
+  const [imageOpacity, setImageOpacity] = useState(0);
+
+  useEffect(() => {
+    setImageOpacity(100);
+  }, []);
+
   return (
     <div
       className={`fixed top-0 left-0 w-full h-full bg-800 bg-noise grid place-items-center z-[1] transition-opacity duration-2000 ${
@@ -10,7 +17,11 @@ const Loader = ({ animationFinished }: { animationFinished: boolean }) => {
       }`}
     >
       <div className="flex flex-col items-center gap-30">
-        <Image className="w-[150px] h-auto" src={logo1} alt="logo" />
+        <Image
+          className={`w-[150px] h-auto transition-opacity duration-700 ease-in-out opacity-${imageOpacity}`}
+          src={logo1}
+          alt="logo"
+        />
         <div
           className={`${mono.variable} font-mono text-white uppercase text-24 leading-20 font-light typewriter whitespace-nowrap`}
         ></div>
