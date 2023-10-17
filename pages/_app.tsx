6bf355d3003eb4b0483fab8628ab4e623d111af9
sotatek-tyler-nguyen <tyler.nguyen3@sotatek.com>
2023-10-17 18:05:28 +0700

    [ECO-740] - Fix connect uncreated wallet show toast message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,8 @@ import "react-loading-skeleton/dist/skeleton.css";
 import { SkeletonTheme } from "react-loading-skeleton";
 import Layout from "@/components/layout";
 import Web3Provider from "@/connect-wallet/provider";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +16,18 @@ export default function App({ Component, pageProps }: AppProps) {
       <Web3Provider>
         <Layout>
           <Component {...pageProps} />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
         </Layout>
       </Web3Provider>
     </SkeletonTheme>
