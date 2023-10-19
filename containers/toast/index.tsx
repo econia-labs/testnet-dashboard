d770881e18c0fc52bfbd6fba0995a5b93d90f8e7
sotatek-tyler-nguyen <145954290+sotatek-tyler-nguyen@users.noreply.github.com>
2023-10-19 14:21:43 +0700

    [ECO-757] - Update toast config

--- containers/toast/index.tsx
+++ containers/toast/index.tsx
@@ -6,8 +6,8 @@ const CloseToastButton = () => {
         <button onClick={closeToast} className="Toastify__close-button Toastify__close-button--dark " type="button" aria-label="close">
             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g id="Add">
-                    <path id="Vector" d="M5.22656 4.31433L13.7163 12.8041" stroke="white" stroke-linecap="square" />
-                    <path id="Vector_2" d="M5.22656 12.8041L13.7163 4.31433" stroke="white" stroke-linecap="square" />
+                    <path id="Vector" d="M5.22656 4.31433L13.7163 12.8041" stroke="white" strokeLinecap="square" />
+                    <path id="Vector_2" d="M5.22656 12.8041L13.7163 4.31433" stroke="white" strokeLinecap="square" />
                 </g>
             </svg>
 
@@ -19,7 +19,7 @@ const ToastContainer = () => {
     return (
         <Container
             position="bottom-right"
-            autoClose={50000000}
+            autoClose={2000}
             hideProgressBar={false}
             newestOnTop={false}
             closeOnClick
