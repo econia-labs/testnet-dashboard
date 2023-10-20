d87a258c70825969ecabd51a11f214d7b9e5ff5f
sotatek-tyler-nguyen <145954290+sotatek-tyler-nguyen@users.noreply.github.com>
2023-10-20 09:32:37 +0700

    [ECO-768] - Update maintenance business (#81)

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,16 +2,10 @@ import React, { useEffect, useState } from 'react'
 import Navbar from './navbar'
 import { mono, jost } from '@/fonts/fonts'
 import Loader from './loader'
-import Maintenance from './maintenance'
-import { healthCheck } from '@/services/axiosSetup'
-const MAX_RETRY = 2
-enum Status {
-    IDLE = 'IDLE', ONLINE = 'ONLINE', MAINTAINED = 'MAINTAINED'
-}
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
     const [animationFinished, setAnimationFinished] = useState(false);
     const [loaderShow, setLoaderShow] = useState(true);
-    const [status, setStatus] = useState<Status>(Status.IDLE)
 
     useEffect(() => {
         const animationTimer = setTimeout(() => {
@@ -26,29 +20,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         }
     }, [])
 
-    useEffect(() => {
-        if (process.env.NEXT_PUBLIC_BACKEND_EXPECTED_ONLINE === 'false') {
-            setStatus(Status.MAINTAINED)
-        } else {
-            const f = async (retry: number) => {
-                if (retry > MAX_RETRY) {
-                    return
-                }
-                try {
-                    const heath = await healthCheck()
-                    if (heath.status === 200) {
-                        setStatus(Status.ONLINE)
-                    }
-
-                } catch (error) {
-                    setStatus(Status.MAINTAINED)
-                    f(retry + 1)
-                }
-            }
-            f(0)
-        }
-
-    }, [])
 
     return (
         <div>
@@ -59,27 +30,15 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
                 }
                 
               `}</style>
-            {
-                status === Status.MAINTAINED ? (
-                    <div className="min-h-[100vh] flex items-center text-white">
-                        <Maintenance />
-                    </div>
-                ) : (
-                    <div className={`${mono.variable} font-mono text-white pt-35 pl-46 pr-31.79 sm:pr-33.71 md:pr-42 lg:px-105`}>
-                        {loaderShow && <Loader animationFinished={animationFinished} />}
-                        {
-                            status === Status.ONLINE && (
-                                <>
-                                    <Navbar />
-                                    {children}
-                                </>
-                            )
-                        }
-
-                    </div>
-                )
-            }
-
+            <div className={`${mono.variable} font-mono text-white pt-35 pl-46 pr-31.79 sm:pr-33.71 md:pr-42 lg:px-105`}>
+                {loaderShow && <Loader animationFinished={animationFinished} />}
+                {
+                    <>
+                        <Navbar />
+                        {children}
+                    </>
+                }
+            </div>
         </div>
 
     )
