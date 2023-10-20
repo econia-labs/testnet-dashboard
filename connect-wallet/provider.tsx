770e5e82082449535f25bd3b2ad6519952222130
sotatek-tyler-nguyen <tyler.nguyen3@sotatek.com>
2023-10-20 16:46:44 +0700

    [ECO-724] - Trigger auto connect

--- connect-wallet/provider.tsx
+++ connect-wallet/provider.tsx
@@ -2,11 +2,40 @@ import { MartianWallet, } from "@martianwallet/aptos-wallet-adapter";
 import { PetraWallet } from "petra-plugin-wallet-adapter";
 import { PontemWallet } from "@pontem/wallet-adapter-plugin";
 import { RiseWallet } from "@rise-wallet/wallet-adapter";
-import { FewchaWallet } from "fewcha-plugin-wallet-adapter"
+// import { FewchaWallet } from "fewcha-plugin-wallet-adapter"
 import {
-    AptosWalletAdapterProvider,
+    AptosWalletAdapterProvider, WalletName, useWallet,
 } from "@aptos-labs/wallet-adapter-react";
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useEffect } from "react";
+
+let t: NodeJS.Timeout | null = null
+const AutoConnect = () => {
+    const { account, connect, ...res } = useWallet()
+    useEffect(() => {
+        if (!account && localStorage.getItem('AptosWalletName')) {
+            const f = async () => {
+                try {
+                    await connect(localStorage.getItem('AptosWalletName') as WalletName)
+                } catch (error) {
+                    if (t) {
+                        clearInterval(t)
+                    }
+                }
+            }
+            f()
+            t = setInterval(f, 100)
+        } else if (t) {
+            clearInterval(t)
+        }
+        return () => {
+            if (t) {
+                clearInterval(t)
+            }
+        }
+    }, [account])
+
+    return null
+}
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
     const wallets = [
         new PetraWallet(),
@@ -20,11 +49,8 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
         <AptosWalletAdapterProvider
             plugins={wallets}
             autoConnect={true}
-        // onError={(error: any) => {
-        //     console.log("Custom error handling", error);
-        //     // setErrorAlertMessage(error);
-        // }}
         >
+            <AutoConnect />
             {children}
         </AptosWalletAdapterProvider>
     );
