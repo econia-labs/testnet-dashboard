5a9970a75d5e75ce2d1deeb0fd490146c3525ca6
sotatek-stephen-pham <stephen.pham@sotatek.com>
2023-10-23 10:28:17 +0700

    Add messages constants

--- components/maintenance/index.tsx
+++ components/maintenance/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import { MAINTENANCE_ERROR } from "@/constants/error-messages";
+import React from "react";
 
 const Maintenance = () => {
-    return (
-        <div className='flex justify-center items-center h-full w-full'>
-            <div className='w-[512px] max-w-full text-center'>
-                <div className="heart mb-[60px]"></div>
-                <h1 className='font-jost font-bold text-32 leading-52 mb-[11.1px]'>Under Maintenance</h1>
-                <p className='text-14 font-mono font-light leading-30'>
-                    The leaderboard backend is offline for maintenance and will be back online shortly. Your data is not lost, because all of the data is indexed straight from the blockchain! 💙
-                </p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center items-center h-full w-full">
+      <div className="w-[512px] max-w-full text-center">
+        <div className="heart mb-[60px]"></div>
+        <h1 className="font-jost font-bold text-32 leading-52 mb-[11.1px]">
+          {MAINTENANCE_ERROR.title}
+        </h1>
+        <p className="text-14 font-mono font-light leading-30">
+          {MAINTENANCE_ERROR.message}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Maintenance
+export default Maintenance;
---
+++ constants/error-messages.ts
@@ -0,0 +1,6 @@
+import { ErrorMessage } from "@/types/error-messages";
+
+export const MAINTENANCE_ERROR: ErrorMessage = {
+    title: "Under Maintenance",
+    message: "The leaderboard backend is offline for maintenance and will be back online shortly. Your data is not lost, because all of the data is indexed straight from the blockchain! 💙"
+}
